4b2172082ee6cbf5445776b4a29e973892bd5960
Adrian Mato <yensamg@gmail.com>
2018-11-23 18:39:04 -0800

    Refactored writing into database

--- app.js
+++ app.js
@@ -4,32 +4,66 @@ let firebase = require("firebase-admin");
 let firebaseAccount = require("./serviceAccountKey.json");
 
 // Configuration
-let endpoints = [
-  { name: "Personal site", address: "adrianmato.com" },
-  { name: "PUBG US West", address: "dynamodb.us-west-1.amazonaws.com" },
-  { name: "Overwatch US West", address: "24.105.30.129" },
-  { name: "Overwatch Korea", address: "211.234.110.1" },
-  { name: "Overwatch Taiwan", address: "203.66.81.98" }
+let endpoints = [{
+    name: "Personal site",
+    address: "adrianmato.com"
+  },
+  {
+    name: "Lucia's website site",
+    address: "luciagm.net"
+  },
+  {
+    name: "PUBG US West",
+    address: "dynamodb.us-west-1.amazonaws.com"
+  },
+  {
+    name: "Overwatch US West",
+    address: "24.105.30.129"
+  },
+  {
+    name: "Overwatch Korea",
+    address: "211.234.110.1"
+  },
+  {
+    name: "Overwatch Taiwan",
+    address: "203.66.81.98"
+  }
 ];
-let collection = "endpoints";
+let db;
+let dbCollection = "endpoints";
+let dbDocument = "feed";
 
 // Run app
 App(endpoints);
 
 // Functions
 async function App(endpoints) {
-  let db;
-
   try {
     db = await dbLogin();
     await pingAll(endpoints);
   } catch (error) {
-    console.log("\n🛑 ERROR! Exiting app…");
+    console.log(`\n🛑 ERROR! Exiting app… (${error})`);
     process.exit();
   }
 
-  await dbSetData(db, collection, "host", "data");
   console.log("\n🏁 Finished!");
+}
+
+async function dbLogin() {
+  firebase.initializeApp({
+    credential: firebase.credential.cert(firebaseAccount)
+  });
+  let db = firebase.firestore();
+
+  if (typeof db !== "undefined") {
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    console.log("🔑 Logged into Firebase");
+    return db;
+  }
+
+  throw Error("🛑 Firebase couldn't be initialized");
 }
 
 async function pingAll(endpoints) {
@@ -41,68 +75,46 @@ async function pingAll(endpoints) {
 }
 
 async function pingEndpoint(host) {
-  let config = { timeout: 1 };
+  let config = {
+    timeout: 1
+  };
   let result = await ping.promise.probe(host, config);
   if (result.alive === true) {
     console.log(`✅ ${result.time}ms \t ${host}`);
   } else console.log(`🔴 failed \t ${host}`);
 
-  return result;
+  await writeEndpoint(result);
 }
 
-async function dbLogin() {
-  firebase.initializeApp({
-    credential: firebase.credential.cert(firebaseAccount)
-  });
-  let db = firebase.firestore();
+async function writeEndpoint(result) {
+  let entry = {
+    document: result.host,
+    response: result.time != "unknown" ? result.time : false,
+    address: result.numeric_host,
+    date: new Date(Date.now())
+  };
+  await console.log(entry);
 
-  if (typeof db !== "undefined") {
-    db.settings({ timestampsInSnapshots: true });
-    console.log("🔑 Logged into Firebase");
-    return db;
+  try {
+    // Add or update existing endpoint
+    let endpoint = db.collection(dbCollection).doc(entry.document);
+    await endpoint.set({
+      name: entry.document,
+      updated: entry.date,
+      status: entry.response,
+      address: entry.address
+    })
+    console.log(`✏️  endpoint added/updated`)
+
+    // Add new entry under the feed of the endpoint
+    let feed = db.collection(dbCollection).doc(entry.document).collection(dbDocument).doc(Date.now().toString());
+    await feed.set({
+      date: entry.date,
+      response: entry.response,
+      address: entry.address
+    })
+    console.log(`✏️  entry added under '${entry.document}'`)
+  } catch (error) {
+    console.log(`\n🛑 ERROR! Couldn't write into database… (${error})`);
   }
-
-  throw Error("🛑 Firebase couldn't be initialized");
-}
-
-async function dbSetData(db, collection, host, data) {
-  // console.log(db);
-}
-
-function firebaseSetData(db, collection, endpoint, data) {
-  // Create an entry in the feed and update parent collection fields
-  let document = endpoint.address;
-
-  return db
-    .collection(collection)
-    .doc(document)
-    .collection("feed")
-    .add({ data })
-    .then(ref => {
-      console.log(`\n✏️  Entry created: ${ref.id}`);
-    })
-    .then(() => {
-      db.collection(collection)
-        .doc(document)
-        .set({
-          name: endpoint.name,
-          updated: data.date,
-          status: data.response != "unknown" ? true : false
-        });
-    });
-}
-
-function firebaseGetData(db, collection) {
-  db.collection(collection)
-    .get()
-    .then(snapshot => {
-      console.log("\n📄 Data retrieved:");
-
-      snapshot.forEach(doc => {
-        console.log(doc.data());
-      });
-    })
-    .catch(error => {
-      console.log(`Error: ${error}`);
-    });
 }
