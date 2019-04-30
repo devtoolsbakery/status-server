c918d04aa6561262e462b3717b1a87b0d47c2a25
Iván Guardado <dev.ivangc@gmail.com>
2019-04-30 22:56:33 +0200

    Improves the unit tests for the PingAllEndponings use case (#18)

--- src/core/domain/usecase/PingAllEndpoints.ts
+++ src/core/domain/usecase/PingAllEndpoints.ts
@@ -1,20 +1,21 @@
 import EndpointStatusRepository from '../model/EndpointStatusRepository';
 import PingService from '../model/service/PingService'
 import EventPublisher from '../model/event/EventPublisher';
-import SavePingResult from '../model/service/savePingResult';
 import EndpointUpdatedEvent from '../model/event/EndpointUpdatedEvent';
 import EndpointStatus from '../model/EndpointStatus';
+import PingResult from '../model/PingResult';
 
 export default class PingAllEndpoints {
   private pingService: PingService;
   private endpointStatusRepository: EndpointStatusRepository
-  private savePingResult: SavePingResult;
   private eventPublisher: EventPublisher
 
-  constructor(endpointStatusRepository: EndpointStatusRepository, savePingResult, pingService: PingService,
-    eventPublisher: EventPublisher) {
+  constructor(
+      endpointStatusRepository: EndpointStatusRepository, 
+      pingService: PingService,
+      eventPublisher: EventPublisher
+    ) {
     this.endpointStatusRepository = endpointStatusRepository;
-    this.savePingResult = savePingResult;
     this.pingService = pingService;
     this.eventPublisher = eventPublisher;
   }
@@ -22,12 +23,19 @@ export default class PingAllEndpoints {
   async execute() {
     const iterator = async (endpointStatus: EndpointStatus) => {
       const pingResult = await this.pingService.ping(endpointStatus);
-      const result = await this.savePingResult.save({ endpointStatus, pingResult });
+      const result = await this.savePingResult(endpointStatus, pingResult);
       this.eventPublisher.publish(EndpointUpdatedEvent.from(endpointStatus));
       return result;
     };
   
     const endpoints = await this.endpointStatusRepository.findAll();
     return Promise.all(endpoints.map(iterator));
+  }
+
+  private savePingResult(endpointStatus: EndpointStatus, pingResult: PingResult) {
+    const address = pingResult.getIp();
+    const time = pingResult.getTimeInMilliseconds();
+    endpointStatus.updateFromPing({ address, time });
+    return this.endpointStatusRepository.save(endpointStatus);
   }
 }
---
+++ test/core/domain/usecase/PingAllEdnpointsUnitTest.ts
@@ -0,0 +1,70 @@
+import { mock, verify, when, instance, anyOfClass, anything } from "ts-mockito";
+import EndpointStatusRepository from "../../../../src/core/domain/model/EndpointStatusRepository";
+import EndpointStatusFirebaseRepository from "../../../../src/core/infrastructure/repository/EndpointStatusFirebaseRepository";
+import PingService from "../../../../src/core/domain/model/service/PingService";
+import EventPublisher from "../../../../src/core/domain/model/event/EventPublisher";
+import PingServiceImpl from "../../../../src/core/infrastructure/PingService";
+import PubSub from "../../../../src/core/infrastructure/PubSub";
+import EndpointStatus from "../../../../src/core/domain/model/EndpointStatus";
+import PingResult from "../../../../src/core/domain/model/PingResult";
+import PingAllEndpoints from "../../../../src/core/domain/usecase/PingAllEndpoints";
+
+const endpoint = new EndpointStatus('1', 'ivanguardado.com', '', 'Ivan Site', 0, null);
+
+export default class PingAllEndpointsUnitTest {
+
+  private mockedEndpointStatusRepository: EndpointStatusRepository = mock(EndpointStatusFirebaseRepository);
+  private mockedPingService: PingService = mock(PingServiceImpl);
+  private mockedPubSub: PubSub = mock(PubSub);
+
+  givenMultipleSuccessfullEndpoints(): void {
+    this.whenRepositoryFind([endpoint, endpoint]);
+    this.whenSuccessfulPing(new PingResult('', '', 100));
+  }
+
+  givenSomeFailedEndpoint() {
+    this.whenRepositoryFind([endpoint, endpoint]);
+    this.whenFailedfulPing();
+  }
+  
+  whenFailedfulPing() {
+    const pingResult = new PingResult('error.com', null, 0);
+    when(this.mockedPingService.ping(anyOfClass(EndpointStatus))).thenResolve(pingResult);
+  }
+
+  eventPublisherShouldEmitEvent() {
+    verify(this.mockedPubSub.publish(anything())).twice();
+  }
+
+  repositoryShouldSave() {
+    verify(this.mockedEndpointStatusRepository.save(anyOfClass(EndpointStatus))).twice();
+  }
+
+  buildPingAllEndpointsUseCase(): PingAllEndpoints {
+    return new PingAllEndpoints(
+      this.getEndpointStatusRepository(),
+      this.getPingService(),
+      this.getEventPublisher()
+    );
+  }
+
+  private getEndpointStatusRepository(): EndpointStatusRepository {
+    return instance(this.mockedEndpointStatusRepository);
+  }
+
+  private getPingService(): PingService {
+    return instance(this.mockedPingService);
+  }
+
+  private getEventPublisher(): EventPublisher {
+    return instance(this.mockedPubSub);
+  }
+
+  private whenRepositoryFind(endpoints: EndpointStatus[]) {
+    when(this.mockedEndpointStatusRepository.findAll()).thenResolve(endpoints);
+  }
+
+  private whenSuccessfulPing(pingResult: PingResult) {
+    when(this.mockedPingService.ping(anyOfClass(EndpointStatus))).thenResolve(pingResult);
+  }
+}
