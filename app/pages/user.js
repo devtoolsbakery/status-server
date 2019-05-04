096d7323970b5f2a267d3ce38b5cf5b8ea9e775b
Adrian Mato <yensamg@gmail.com>
2019-05-03 22:18:42 -0700

    Added Card component, loading default theme stles

--- app/pages/user.js
+++ app/pages/user.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import Card from '../components/Card';
+
+import '../styles/pages/user.css';
+
 
 class User extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      username : 'Company server',
+      theme : 'monkey' // example: use 'monkey' or 'minimal'
+    }
+  }
+
   render () {
     return (
       <>
+      <Card name={`${this.state.username}`} status='online' dayLimit='90' />
+      {/* <>
         User page, the theme loaded is: {this.props.theme}
+      </> */}
       </>
     )
   }
