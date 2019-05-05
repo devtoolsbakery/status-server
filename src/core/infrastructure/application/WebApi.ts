415a6d2bbf52b3627dfcc4cd11807618a1805ec5
Adrian Mato <yensamg@gmail.com>
2019-05-05 14:43:13 -0700

    Enabled CORS for all domains on WebApi

--- src/core/infrastructure/application/WebApi.ts
+++ src/core/infrastructure/application/WebApi.ts
@@ -1,5 +1,6 @@
 import Application from "./Application";
 import * as express from 'express';
+import * as cors from 'cors';
 import container from '../DependencyInjection';
 import ApiController from "../http/express/ApiController";
 
@@ -7,6 +8,7 @@ export default class WebApi implements Application {
   
   run() {
     const app = express();
+    app.use(cors());
     const apiController = new ApiController();
     
     app.get('/:username/endpoints', apiController.getUserEndpoints);
